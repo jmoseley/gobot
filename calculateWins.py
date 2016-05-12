c7906d144f6c160d9f8220eda25e9efc02787991
Jeremy Moseley <jeremy@jeremymoseley.net>
2016-05-11 22:42:16 -0700

    Better lookup table to reduce collisions. Incorporate board edge for correct player 1 win checking.

--- calculateWins.py
+++ calculateWins.py
@@ -1,23 +1,26 @@
 import sys
-def calcH(p1, p2, p3, p4, p5):
-  print '%s %s %s %s %s' % (p1, p2, p3, p4, p5)
-  return p1 << 8 ^ p2 << 6 ^ p3 << 4 ^ p4 << 2 ^ p5
+def calcH(p0, p1, p2, p3, p4, p5, p6):
+  return p0 << 12 ^ p1 << 10 ^ p2 << 8 ^ p3 << 6 ^ p4 << 4 ^ p5 << 2 ^ p6
 
 MAX_INT = sys.maxint
 
-LOOKUP = [0]*690
+LOOKUP = [0]*10930
 for player in range(2):
   color = player + 1
-  for p1 in range(2):
-    for p2 in range(2):
-      for p3 in range(2):
-        for p4 in range(2):
-          for p5 in range(2):
-            h = calcH(p1 * color, p2 * color, p3 * color, p4 * color, p5 * color)
-            val = 10 ** (p1 + p2 + p3 + p4 + p5)
-            if p1 == p2 and p2 == p3 and p3 == p4 and p4 == p5 and p1 != 0:
-              val = MAX_INT
-            LOOKUP[h] = val
+  for p0 in range(2):
+    for p1 in range(2):
+      for p2 in range(2):
+        for p3 in range(2):
+          for p4 in range(2):
+            for p5 in range(2):
+              for p6 in range(2):
+                if (color == 1 and p0 == 0 and p6 == 0) or color == 2:
+                  h = calcH(p0*color, p1 * color, p2 * color, p3 * color, p4 * color, p5 * color, p6*color)
+                  exponent = (p0 + p1 + p2 + p3 + p4 + p5 + p6)
+                  if exponent >= 2:
+                    val = 10 ** exponent
+                    if p1 == p2 and p2 == p3 and p3 == p4 and p4 == p5 and p1 != 0:
+                      val = MAX_INT
+                    LOOKUP[h] = val
 
 print LOOKUP
-print LOOKUP[calcH(1, 2, 0, 1, 1)]
