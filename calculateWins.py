eec466015c0d4c40c10564e9887803d01f8e57ea
Jeremy Moseley <jeremy@jeremymoseley.net>
2016-05-15 13:51:30 -0700

    Got me 57th place. Out of 1600.

--- calculateWins.py
+++ calculateWins.py
@@ -7,20 +7,26 @@ MAX_INT = sys.maxint
 LOOKUP = [0]*10930
 for player in range(2):
   color = player + 1
-  for p0 in range(2):
-    for p1 in range(2):
-      for p2 in range(2):
-        for p3 in range(2):
-          for p4 in range(2):
-            for p5 in range(2):
-              for p6 in range(2):
-                if (color == 1 and p0 == 0 and p6 == 0) or color == 2:
-                  h = calcH(p0*color, p1 * color, p2 * color, p3 * color, p4 * color, p5 * color, p6*color)
-                  exponent = (p0 + p1 + p2 + p3 + p4 + p5 + p6)
-                  if exponent >= 2:
-                    val = 10 ** exponent
-                    if p1 == p2 and p2 == p3 and p3 == p4 and p4 == p5 and p1 != 0:
-                      val = MAX_INT
-                    LOOKUP[h] = val
+  for p1 in range(2):
+    for p2 in range(2):
+      for p3 in range(2):
+        for p4 in range(2):
+          for p5 in range(2):
+            exponent = (p1 + p2 + p3 + p4 + p5)
+            if exponent >= 3:
+              val = 10 ** exponent
+              if exponent == 5:
+                val = MAX_INT
+              h = calcH(0, p1 * color, p2 * color, p3 * color, p4 * color, p5 * color, 0)
+              LOOKUP[h] = val
+              for borderPlayer in range(2):
+                borderColor = borderPlayer + 1
+                if (color == 1 and borderColor != 1) or color == 2:
+                  h = calcH(1 * borderColor, p1 * color, p2 * color, p3 * color, p4 * color, p5 * color, 0)
+                  LOOKUP[h] = val
+                  h = calcH(0, p1 * color, p2 * color, p3 * color, p4 * color, p5 * color, 1 * borderColor)
+                  LOOKUP[h] = val
+                  h = calcH(1 * borderColor, p1 * color, p2 * color, p3 * color, p4 * color, p5 * color, 1 * borderColor)
+                  LOOKUP[h] = val
 
 print LOOKUP
